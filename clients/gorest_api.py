ee334eb5c877f8d2c4d03f6d1c91d4be50db0e70
mlamamra <mlamamra@dawan.fr>
2026-01-05 15:32:17 +0100

    client python requests: call get en POO avec encapsulation

--- clients/gorest_api.py
+++ clients/gorest_api.py
@@ -29,5 +29,33 @@ Bonus: travailler avec une session requests pour optimiser les appels répétés
 ## gestion des erreurs HTTP et des exceptions réseau et status code et header Content-Type et gestion du JSON
 
 import requests
+from requests.exceptions import ConnectionError, HTTPError, Timeout, RequestException
+
+nb_results = 10
+page_number = 2
+
+try:
+  response = requests.get(
+    "https://gorest.co.in/public/v2/users", 
+    params={"page": page_number, "per_page": nb_results}
+  )
+  if 200 <= response.status_code < 300:
+    # gestion de la forme des données reçues
+    if "Content-Type" in response.headers and "application/json" in response.headers["Content-Type"]:
+      data = response.json()
+      print(data)
+  # gestion des codes HTTP d'erreur
+  else:
+    # raise HTTPError(f"erreur HTTP: {response.status_code}")
+    response.raise_for_status()
+## gérer plusieurs exceptions réseau avec le même bloc except
+except (ConnectionError, RequestException) as e:
+  print("erreur réseau:", e)
 
 
+# %%
+## utilisation de requests avec POO
+# 1/ créer une classe GoRestClient
+# 2/ gérer les paramètres d'instanciation du client dans le __init__
+# 3/ créer une méthode publique get_users_page pour insérer les paramétres et retourner la réponse
+# 4/ créer une méthode privée __call qui manipule requests
